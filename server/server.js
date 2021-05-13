696cb5676cf64807f96c1a8084b10fe35a02aa78
Cody <74194707+lewisoutdoorllc@users.noreply.github.com>
2021-05-13 13:49:40 -0400

    quick update

--- server/server.js
+++ server/server.js
@@ -1,20 +1,20 @@
-const express = require('express');
+const express = require("express");
 // import ApolloServer
-const {ApolloServer} = require('apollo-server-express');
+const { ApolloServer } = require("apollo-server-express");
 // import our typeDefs and resolvers
-const { typeDefs, resolvers } = require('./schemas');
-const path = require('path');
-const db = require('./config/connection');
-const routes = require('./routes');
-const {authMiddleware} = require('./utils/auth');
+const { typeDefs, resolvers } = require("./schemas");
+const path = require("path");
+const db = require("./config/connection");
+const routes = require("./routes");
+const { authMiddleware } = require("./utils/auth");
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 // create a new Apollo server and pass in our schema data
-const server = new ApolloServer({ 
-  typeDefs, 
-  resolvers, 
-  context: authMiddleware 
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: authMiddleware,
 });
 // integrate our Apollo server with the Express application as middleware
 server.applyMiddleware({ app });
@@ -23,12 +23,14 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // if we're in production, serve client/build as static assets
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../client/build")));
 }
 
 app.use(routes);
 
-db.once('open', () => {
+db.once("open", () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+  // log where we can go to test our GQL API
+  console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
 });
