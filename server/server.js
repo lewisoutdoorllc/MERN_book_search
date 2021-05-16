e84218cd0baeaac24b7ba98b56091b8403f7587e
Cody <74194707+lewisoutdoorllc@users.noreply.github.com>
2021-05-16 14:20:02 -0400

    re ran npm install; testing again

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ const { ApolloServer } = require("apollo-server-express");
 const { typeDefs, resolvers } = require("./schemas");
 const path = require("path");
 const db = require("./config/connection");
-const routes = require("./routes");
+// const routes = require("./routes");
 const { authMiddleware } = require("./utils/auth");
 
 const PORT = process.env.PORT || 3001;
